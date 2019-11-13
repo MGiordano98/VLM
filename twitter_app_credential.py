dcbeaae97eeb86e87bcb6f6435ecd0ef9995307a
Marti <vittoriobaldi18@gmail.com>
2019-11-13 11:20:43 +0100

    Si

--- twitter_app_credential.py
+++ twitter_app_credential.py
@@ -1,7 +1,24 @@
 # my twitter app credentials
+consumer_key = "DvIJROlZmX32RctFrpQSRlHYj"
+consumer_secret = "NG2rnPBQ2N26Z6PWfCNz5tUkthotDcjokb6T1CZTI5RqwmrQA1"
 
-consumer_key = ""
-consumer_secret = ""
+access_token = "1192747749990653952-CxqEA7iUvRx1N468HlZWxGUW1rkCSD"
+access_token_secret = "5ndTXJx4hRfrMO4QrqN95YCIvAsXyi3SiAdEPkZmeO3g2"
 
-access_token = ""
-access_token_secret = ""
+# Import the Twython class
+from twython import Twython
+import json
+
+# Load credentials from json file
+with open("twitter_credentials.json", "r") as file:
+    creds = json.load(file)
+
+# Instantiate an object
+python_tweets = Twython(creds[consumer_key], creds[consumer_secret])
+
+# Create our query
+query = {'q': 'learn python',
+        'result_type': 'popular',
+        'count': 10,
+        'lang': 'en',
+        }
