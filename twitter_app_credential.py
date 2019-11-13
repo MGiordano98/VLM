4afea8d51c09daec477bdb7cc36cf9184a0da82c
Marti <vittoriobaldi18@gmail.com>
2019-11-13 11:41:06 +0100

    Prova

--- twitter_app_credential.py
+++ twitter_app_credential.py
@@ -1,24 +1,14 @@
 # my twitter app credentials
-consumer_key = "DvIJROlZmX32RctFrpQSRlHYj"
-consumer_secret = "NG2rnPBQ2N26Z6PWfCNz5tUkthotDcjokb6T1CZTI5RqwmrQA1"
 
-access_token = "1192747749990653952-CxqEA7iUvRx1N468HlZWxGUW1rkCSD"
-access_token_secret = "5ndTXJx4hRfrMO4QrqN95YCIvAsXyi3SiAdEPkZmeO3g2"
-
-# Import the Twython class
-from twython import Twython
 import json
 
-# Load credentials from json file
-with open("twitter_credentials.json", "r") as file:
-    creds = json.load(file)
+# Enter your keys/secrets as strings in the following fields
+credentials = {}
+credentials['CONSUMER_KEY'] = "DvIJROlZmX32RctFrpQSRlHYj"
+credentials['CONSUMER_SECRET'] = "NG2rnPBQ2N26Z6PWfCNz5tUkthotDcjokb6T1CZTI5RqwmrQA1"
+credentials['ACCESS_TOKEN'] = "1192747749990653952-CxqEA7iUvRx1N468HlZWxGUW1rkCSD"
+credentials['ACCESS_SECRET'] = "5ndTXJx4hRfrMO4QrqN95YCIvAsXyi3SiAdEPkZmeO3g2"
 
-# Instantiate an object
-python_tweets = Twython(creds[consumer_key], creds[consumer_secret])
-
-# Create our query
-query = {'q': 'learn python',
-        'result_type': 'popular',
-        'count': 10,
-        'lang': 'en',
-        }
+# Save the credentials object to file
+with open("twitter_credentials.json", "w") as file:
+    json.dump(credentials, file)
