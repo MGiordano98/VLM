82e4ccd40c47769156af40258c0fff0c73def6ef
VittorioBaldi <vittoriobaldi18@gmail.com>
2019-11-16 16:02:10 +0100

    Ah si giusto, una coca cola

--- showTweet.py
+++ showTweet.py
@@ -3,8 +3,13 @@ import csv
 import pandas as pd
 
 tweets_nutella_biscuits = "NutellaBiscuits/tweets_nutella_biscuits.csv"
+ABH = "ABH/tweets_ABH.csv"
 
 
 df = pd.read_csv(tweets_nutella_biscuits, names=[ 'screen_name','text','date', 'favorite_count', 'retweet_count', 'location'])
+df.sort_values(by='date', inplace=True, ascending=False)
+df
+
+df = pd.read_csv(tweets_ABH, names=[ 'screen_name','text','date', 'favorite_count', 'retweet_count', 'location'])
 df.sort_values(by='date', inplace=True, ascending=False)
 df
