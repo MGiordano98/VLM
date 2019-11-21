af21526e405dbaf1b4b425f0a04b0ef8a2fa6c8c
MGiordano98 <maximo98.mg@gmail.com>
2019-11-21 10:55:36 +0100

    si

--- FrequencyWords/frequency.py
+++ FrequencyWords/frequency.py
@@ -6,53 +6,21 @@ import getMethods
 
 hashtags = getMethods.getHashtags()
 
-tweets= getMethods.getTweets()
-
-words = []
-
-for key,value in tweets.items():
-    x = []
-    for text in value["text"]:
-        if 'RT @' in text:
-            x.append(text)
-        else:
-            y.append(tweets[key]["screen_name"])
-    tweets_RT[key] = x
-    tweets_NoRT[key] = y
- 
-
-
-stopwords_it = nltk.corpus.stopwords.words('italian')
-stopwords_en = nltk.corpus.stopwords.words('english')
-stopwords = stopwords_it + stopwords_en
-RE_stopwords = r'\b(?:{})\b'.format('|'.join(stopwords))
+tweets = getMethods.getTweets()
 
 
 
 words =  {}
 wordsRT = {}
 dizionario = {}
-for key,value in tweets_RT.items():
+for i,val in enumerate(hashtags):
     x = []
-    for t in value:
-        y = t.split()
-        x.append(y[1])
-    wow = getMethods.getRslt(x)
-    wordsRT[key] = wow
-
-""" for i,val in enumerate(hashtags):
-    x = []
-    for t in tweetsoRT[val]:
+    for t in tweets[val]["text"]:
         for i in t.split():
-            if i.startswith('@'):
+            if i.startswith('@') and i.endswith(':'):
                 x.append(i)
     wow = getMethods.getRslt(x)
-    words.append(wow)
-
-
-for i,val in enumerate(hashtags):
-    print(words[val]) """
-
+    wordsRT[val] = wow
 
 
 for i,val in enumerate(hashtags):
